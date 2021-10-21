d5f2eede21e57d93845e688dc62782ec8049be80
danielaciuperca <daniela.ciuperca20@gmail.com>
2021-10-21 10:18:20 +0300

    Curs 3

---
+++ Projects/Projects_Course_3/ex2curs3/src/main/java/musicstore/Album.java
@@ -0,0 +1,40 @@
+package musicstore;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Component
+public class Album {
+    private String artist;
+    private LocalDate releaseDate;
+    private double price;
+
+    public Album() {
+        System.out.println("Album constructor was called");
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
