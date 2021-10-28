ead0e4923fa9da76a6f34a8ef6b183b80b3dcd18
danielaciuperca <daniela.ciuperca20@gmail.com>
2021-10-28 10:01:31 +0300

    Curs 4

---
+++ Projects/Projects_course_4/ex3curs4/src/main/java/com/unibuc/ex3curs4/Ex3curs4Application.java
@@ -0,0 +1,13 @@
+package com.unibuc.ex3curs4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex3curs4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex3curs4Application.class, args);
+    }
+
+}
---
+++ Projects/Projects_course_4/ex3curs4/src/test/java/com/unibuc/ex3curs4/Ex3curs4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unibuc.ex3curs4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex3curs4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
